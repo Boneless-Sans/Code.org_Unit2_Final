216613de831d1016097609d8f937602968a8fe4c
Boneless-Sans <bonelessmail0101@gmail.com>
2023-11-02 13:50:24 -0700

    init

--- src/com/boneless/unit2Final/Main.java
+++ src/com/boneless/unit2Final/Main.java
@@ -3,6 +3,8 @@ package com.boneless.unit2Final;
 import com.boneless.unit2Final.util.FileReaderSaver;
 
 import javax.swing.*;
+import java.awt.*;
+
 public class Main extends JFrame {
     private static JFrame mainMenu;
     public static void main(String[] args) {
@@ -10,8 +12,15 @@ public class Main extends JFrame {
         mainMenu.setSize(500,500);
         mainMenu.setLayout(null);
 
-        System.out.println(FileReaderSaver.extractNamesWithNumber("employees.json", "12345"));
+        JTextField employeeNum = new JTextField();
+        employeeNum.setBounds(0,0,300,200);
+        employeeNum.setFont(new Font("Arial", Font.PLAIN,20));
 
+        JButton submit = new JButton("Submit");
+
+        System.out.println(FileReaderSaver.extractNamesWithNumber("employees.json", employeeNum.getText()));
+
+        mainMenu.add(employeeNum);
         mainMenu.setVisible(true);
     }
 }
